5acc467f2147752321d13e9cf1d2bc8a6da74bf0
english-teaching <blkumawat120878@gmail.com>
2026-09-21 12:29:32 +0530

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const PDFs = [
     category: "Class 10",
     subject: "Hindi",
     chapter: "Chapter 1",
-    url: "https://drive.google.com/file/d/1lMn4V0QxjDUdaESacaHCHh9bu_8rwqV5/view?usp=drivesdk",
+    url: "https://archive.org/download/active-passive/active_passive.pdf",
     downloadUrl: ""
   },
   {
